c73a1607ebd77db0d7a3c23c8799199166fd1145
Alexander Cheryomukhin <solarwind.palm@gmail.com>
2023-10-25 21:44:25 +0300

    Add API prototype and Swagger

---
+++ api/v1/main.go
@@ -0,0 +1,42 @@
+package v1
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	_ "trava/api/docs"
+
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+)
+
+// statusCheck godoc
+// @Summary      Server status
+// @Description  Get server status
+// @Produce      json
+// @Success      200
+// @Router       /status [get]
+func checkStatus(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "OK",
+	})
+}
+
+// @title          Trava API
+// @version        1.0
+// @description    Trava API documentation server
+// @contact.name   Alexander Cheryomukhin
+// @contact.email  solarwind.palm@gmail.com
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+// @host      localhost:8080
+// @BasePath  /api/v1
+func RunRouter() {
+	router := gin.Default()
+	router.GET("/api/v1/status", checkStatus)
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	router.Run()
+}
--- cmd/trava/main.go
+++ cmd/trava/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	apiV1 "trava/api/v1"
+)
 
 func main() {
 	fmt.Println("Trava 0.0.1")
+
+	apiV1.RunRouter()
 }
